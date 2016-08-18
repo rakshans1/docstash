88d912beed6f5bbfa950094d08fd5e29dd6aa408
Rakshan <shetty.raxx555@gmail.com>
2016-08-18 20:13:05 +0530

    updated Dev Server

--- client/tools/srcServer.js
+++ client/tools/srcServer.js
@@ -4,9 +4,13 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from '../webpack.config.dev';
+import Dashboard from 'webpack-dashboard';
+import  DashboardPlugin from 'webpack-dashboard/plugin';
 
 /* eslint-disable no-console */
 const bundler = webpack(config);
+const dashboard = new Dashboard();
+bundler.apply(new DashboardPlugin(dashboard.setData));
 
 browserSync({
   port: 3000,
@@ -25,7 +29,7 @@ browserSync({
 
         // These settings suppress noisy webpack output so only errors are displayed to the console.
         noInfo: false,
-        quiet: false,
+        quiet: true, //was false changed for webpack-dashboard
         stats: {
           assets: false,
           colors: true,
@@ -41,7 +45,9 @@ browserSync({
       }),
 
       // bundler should be the same as above
-      webpackHotMiddleware(bundler)
+      webpackHotMiddleware(bundler,{
+        log: () => {}
+      })
     ]
   },
 
